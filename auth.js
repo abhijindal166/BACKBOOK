5afb0d559de7d3f3cd1799feccb568513ece4e3f
abhijindal166 <106230659+abhijindal166@users.noreply.github.com>
2023-08-16 01:22:19 +0530

    Update auth.js

--- auth.js
+++ auth.js
@@ -1,26 +1,41 @@
-const jwt=require("jsonwebtoken");
-const Post = require("./schema2")
-const auth=async(req,res,next)=>{
-    try{
-      
-        // console.log("entering middleware");
-const token=req.cookies.jwtoken;
-const verify=jwt.verify(token,"abhishekjindalabhishekjindal");
-const rootuser=await Post.findOne({_id:verify._id,"tokens.token":token});
-if(!rootuser){
-    throw new Error("user not found")
-}
-req.token=token;
-req.rootuser=rootuser;
-req.userid=rootuser._id;
-console.log("heree");
-next();
-    }
-    catch(err){
-        res.status(401).send("unauthorized");
-        console.log("error in middleware");
-        console.log(err);
+public class Customer {
+    private int customerId;
+    private String name;
+    private String mailId;
+    private String contact;
+    private String accountType;
+
+    public Customer(int customerId, String name, String mailId, String contact, String accountType) {
+        this.customerId = customerId;
+        this.name = name;
+        this.mailId = mailId;
+        this.contact = contact;
+        this.accountType = accountType;
     }
 
-}
-module.exports=auth;
+    public int getCustomerId() {
+        return customerId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getMailId() {
+        return mailId;
+    }
+
+    public String getContact() {
+        return contact;
+    }
+
+    public String getAccountType() {
+        return accountType;
+    }
+
+    public static void main(String[] args) {
+        Customer[] customers = new Customer[3];
+        
+        customers[0] = new Customer(1, "Alice", "alice@example.com", "1234567890", "Savings");
+        customers[1] = new Customer(2, "Bob", "bob@example.com", "
+        
